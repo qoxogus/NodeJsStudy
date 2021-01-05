def591c967cf0e1300e1ff084726dec677e0a2c5
qoxogus <a01034367055@gmail.com>
2021-01-06 01:38:16 +0900

    [NodeJs Mysql로 홈페이지 구현]

--- main.js
+++ main.js
@@ -6,6 +6,14 @@ var qs = require('querystring');
 var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
+var mysql = require('mysql2');
+var db = mysql.createConnection({
+  host    : 'localhost',
+  user    : 'root',
+  password: '11111111',
+  database: 'opentutorials'
+});
+db.connect();
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -13,13 +21,14 @@ var app = http.createServer(function(request,response){
     var pathname = url.parse(_url, true).pathname;
     if(pathname === '/'){
       if(queryData.id === undefined){
-        fs.readdir('./data', function(error, filelist){
+        db.query(`SELECT * FROM topic`, function(error, topics) {
+          console.log(topics);
           var title = 'Welcome';
           var description = 'Hello, Node.js';
-          var list = template.list(filelist);
+          var list = template.list(topics);
           var html = template.HTML(title, list,
-            `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a>`
+              `<h2>${title}</h2>${description}`, //body
+              `<a href="/create">create</a>` //control
           );
           response.writeHead(200);
           response.end(html);

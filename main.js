322c2f7c1931bb867a133dab6853634aa982a054
qoxogus <a01034367055@gmail.com>
2021-01-13 08:57:46 +0900

    [MySQL join을 이용해서 글 수정 구현]

--- main.js
+++ main.js
@@ -158,6 +158,9 @@ var app = http.createServer(function(request,response){
                   <textarea name="description" placeholder="description">${topic[0].description}</textarea>
                 </p>
                 <p>
+                  ${template.authorSelect(authors, topic[0].author_id)}
+                </p>
+                <p>
                   <input type="submit">
                 </p>
               </form>
@@ -178,15 +181,10 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
           var post = qs.parse(body);
-          var id = post.id;
-          var title = post.title;
-          var description = post.description;
-          fs.rename(`data/${id}`, `data/${title}`, function(error){
-            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-              response.writeHead(302, {Location: `/?id=${title}`});
-              response.end();
-            })
-          });
+          db.query('UPDATE topic SET title=?, description=?, author_id=? WHERE id=?', [post.title, post.description, post.author, post.id], function(error, result){
+            response.writeHead(302, {Location: `/?id=${post.id}`});
+            response.end();
+          })
       });
     } else if(pathname === '/delete_process'){
       var body = '';
